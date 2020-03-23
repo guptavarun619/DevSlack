cf4bcdb5005bd909a067c116ac8d92589f80de4d
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-23 20:56:11 +0530

    Added message functionality

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
+
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
 
 export class Messages extends Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
   render() {
-    return <div>Messages</div>;
+    const { messagesRef, channel, user } = this.state;
+    return (
+      <Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
+      </Fragment>
+    );
   }
 }
 
