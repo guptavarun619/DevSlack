3069d6eef4f07f0257b18e363f10f67cef410760
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-23 15:31:48 +0530

    Add Channel functionality up

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -15,7 +15,7 @@ export class SidePanel extends Component {
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
         <UserPanel currentUser={currentUser} />
-        <Channels />
+        <Channels currentUser={currentUser} />
       </Menu>
     );
   }
