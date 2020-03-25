95274d094f8c35daeb5a54b7e91355917c9906c6
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-25 14:56:01 +0530

    Added Direct Message channel support

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import firebase from "../../firebase";
+import { connect } from "react-redux";
+import { setCurrentChannel, setPrivateChannel } from "../../actions";
 import { Menu, Icon } from "semantic-ui-react";
 
 class DirectMessages extends Component {
@@ -66,6 +68,23 @@ class DirectMessages extends Component {
 
   isUserOnline = (user) => user.status === "online";
 
+  changeChannel = (user) => {
+    const channelId = this.getChannelId(user.uid);
+    const channelData = {
+      id: channelId,
+      name: user.name
+    };
+    this.props.setCurrentChannel(channelData);
+    this.props.setPrivateChannel(true);
+  };
+
+  getChannelId = (userId) => {
+    const currentUserId = this.state.user.uid;
+    return userId < currentUserId
+      ? `${userId}/${currentUserId}`
+      : `${currentUserId}/${userId}/`;
+  };
+
   render() {
     const { users } = this.state;
     return (
@@ -79,7 +98,7 @@ class DirectMessages extends Component {
         {users.map((user) => (
           <Menu.Item
             key={user.uid}
-            onClick={() => console.log(user)}
+            onClick={() => this.changeChannel(user)}
             style={{ opacity: 0.7, fontStyle: "italic" }}
           >
             <Icon
@@ -94,4 +113,6 @@ class DirectMessages extends Component {
   }
 }
 
-export default DirectMessages;
+export default connect(null, { setCurrentChannel, setPrivateChannel })(
+  DirectMessages
+);
