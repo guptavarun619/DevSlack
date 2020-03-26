a5277459b81728dcdcc8b188462cc629af00f64d
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-26 16:44:55 +0530

    Added feature to STAR a channel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -4,6 +4,7 @@ import { Menu } from "semantic-ui-react";
 import UserPanel from "./UserPanel";
 import Channels from "./Channels";
 import DirectMessages from "./DirectMessages";
+import Starred from "./Starred";
 
 export class SidePanel extends Component {
   render() {
@@ -17,6 +18,7 @@ export class SidePanel extends Component {
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
         <UserPanel currentUser={currentUser} />
+        <Starred currentUser={currentUser} />
         <Channels currentUser={currentUser} />
         <DirectMessages currentUser={currentUser} />
       </Menu>
